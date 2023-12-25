483e1e812e09a09d9551c4d3fbc58477b1dd508c
YoungWan Kwon <87213416+000wan@users.noreply.github.com>
2023-12-25 22:09:20 +0900

    Add image support with gemini vision

--- ai.py
+++ ai.py
@@ -7,27 +7,51 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 # Configure Google AI API
 import google.generativeai as genai
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+
+from PIL import Image
+import requests
+from io import BytesIO
 
 def generate_comment(article) -> str:
     instruction = "다음 게시물에 적절한 댓글을 작성하세요. 당신은 KAIST 교내 커뮤니티 Ara의 사용자입니다. 인터넷 예절을 지키되 자연스러운 댓글을 작성하세요. 닉네임을 제외하고 오직 댓글 내용만을 작성하세요."
     
+    content = ara.format_article(article)
     prompt = f'''{instruction}
 
-  게시물: {article}
+    {content}
+    - 나의 댓글: '''
 
-  댓글 내용: '''
+    print(prompt)
 
-    # print(prompt)
+    
+    # Use Gemini Pro Vision if there are attachments
+    attachments = list(filter(lambda attachment: attachment['mimetype'].startswith('image'), article['attachments']))
+    if attachments:
+        model = genai.GenerativeModel('gemini-pro-vision')
+        images = []
+        for attachment in attachments:
+            response = requests.get(attachment['file'])
+            images.append(Image.open(BytesIO(response.content)))
+        #     images[-1].show()
+        # print(images)
 
-    parameters = genai.types.GenerationConfig(
-        candidate_count=1,
-        temperature=0.9,
-        top_p=0.9,
-        top_k=40,
-    )
+        parameters = genai.types.GenerationConfig(
+            candidate_count=1,
+            temperature=0.4,
+            top_p=0.9,
+            top_k=40,
+        )
+        response = model.generate_content([prompt] + images, generation_config=parameters)
+    else:
+        model = genai.GenerativeModel('gemini-pro')
+        parameters = genai.types.GenerationConfig(
+            candidate_count=1,
+            temperature=0.9,
+            top_p=0.9,
+            top_k=40,
+        )
+        response = model.generate_content(prompt, generation_config=parameters)
 
-    response = model.generate_content(prompt, generation_config=parameters)
     try:
         return response.text
     except Exception as e:
@@ -47,7 +71,7 @@ def write_comment(article) -> bool:
         return False
     print(f'* Commenting on article {id}...')
 
-    comment = generate_comment(ara.format_article(article))
+    comment = generate_comment(article)
     res = ara.post_comment(id, comment)
     if res:
         print(f'- Successfully commented on {id}: {comment}\n')
--- test.py
+++ test.py
@@ -3,9 +3,10 @@ def test_comment(article_id):
     import ai
 
     article = ara.get_article(article_id, from_view="board", override_hidden="true")
+    # print(ara.format_article(article))
     res = ai.generate_comment(article)
     print("    " + res)
 
 # test.py
 if __name__ == "__main__":
-    test_comment(5563)
+    test_comment(4528)
